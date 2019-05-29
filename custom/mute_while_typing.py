49e2b6554a8cdefb8497bb7526f161a0282664fa
Kevin Kaland <kevin@wizonesolutions.com>
2019-05-29 15:45:28 +0200

    Fixing mute_when_typing didn't work.

--- custom/mute_while_typing.py
+++ custom/mute_while_typing.py
@@ -1,18 +1,19 @@
-from talon import actions, cron, tap, cron
-
-job = None
-
-def timeout():
-    global job
-    job = None
-    actions.speech.enable()
-
-def on_key(e):
-    global job
-    if e.keyboard_type != 198 and (job or actions.speech.enabled()):
-        actions.speech.disable()
-        if job:
-            cron.cancel(job)
-        job = cron.after('500ms', timeout)
-
-tap.register(tap.KEY, on_key)
+# from talon import actions, cron, tap
+#
+# job = None
+#
+# def timeout():
+#     global job
+#     job = None
+#     cron.after('0s', actions.speech.enable)
+#
+# def on_key(e):
+#     global job
+#     if e.keyboard_type != 198 and (job or actions.speech.enabled()):
+#         cron.after('0s', actions.speech.disable)
+#
+#         if job:
+#             cron.cancel(job)
+#         job = cron.after('500ms', timeout)
+#
+# tap.register(tap.KEY, on_key)
